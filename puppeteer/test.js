f01beb0a631bd4627a454a99c82e84b647825b4e
sefeek <safeek@ontoborn.com>
2022-06-08 09:46:54 +0000

    commit 8

--- puppeteer/test.js
+++ puppeteer/test.js
@@ -3,7 +3,7 @@ const puppeteer = require('puppeteer');
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('https://8080-aacdadadbbcfbbdbadfeecdcaceefaffcabd.ide.exam.ly');
+  await page.goto('https://8080-aacdadadbbcfbbdbadfedacadbbeeafd.ide.exam.ly');
   console.log('TESTCASE:test_case3:success');
   await page.screenshot({path: 'example.png'});
   console.log('TESTCASE:test_case4:success');
--- screenshots/test.js
+++ screenshots/test.js
@@ -12,7 +12,7 @@ var fs = require('fs');
 
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('https://8080-aacdadadbbcfbbdbadfeecdcaceefaffcabd.ide.exam.ly');
+  await page.goto('https://8080-aacdadadbbcfbbdbadfedacadbbeeafd.ide.exam.ly');
   await page.screenshot({path: './output/example.png'});
   
   const image1 = fs.readFileSync("./screenshots/example.png");
@@ -25,7 +25,7 @@ var fs = require('fs');
   console.log('TESTCASE:test_case7:success:',diffpercent);
 
   const page1 = await browser.newPage();
-  await page1.goto('https://8081-aacdadadbbcfbbdbadfeecdcaceefaffcabd.ide.exam.ly');
+  await page1.goto('https://8081-aacdadadbbcfbbdbadfedacadbbeeafd.ide.exam.ly');
   await page1.screenshot({path: './output/example1.png'});
   
   const image3 = fs.readFileSync("./screenshots/example1.png");
